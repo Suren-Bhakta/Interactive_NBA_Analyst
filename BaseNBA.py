ec66de16895179a53d6176a4253fa9532c209d04
Suren Bhakta <113945491+Suren-Bhakta@users.noreply.github.com>
2023-06-26 14:43:49 -0500

    Removed CSV / loop until stop

--- BaseNBA.py
+++ BaseNBA.py
@@ -58,37 +58,37 @@ def visualize_player_stats(player_stats):
     plt.tight_layout()  # Adjust layout to prevent label overlapping
     plt.show()
 
-def export_player_stats(player_stats, filename):
-    player_stats.to_csv(filename, index=False)
-    print(f"Player stats exported to {filename} successfully.")
-
 def main():
     print("Welcome to NBA Player Stats Analyzer!")
-    player_name = input("Enter the name (or partial name) of the player you want to search for: ")
-    search_results = search_players(player_name)
+    while True:
+        player_name = input("Enter the name (or partial name) of the player you want to search for (or type 'stop' to exit): ")
+        if player_name.lower() == "stop":
+            break
 
-    if not search_results:
-        print("No players found.")
-        return
+        search_results = search_players(player_name)
 
-    print("Search Results:")
-    for i, result in enumerate(search_results):
-        print(f"{i+1}. {result['Name']}")
+        if not search_results:
+            print("No players found.")
+            continue
 
-    player_index = int(input("Enter the index of the player you want to analyze: ")) - 1
-    selected_player = search_results[player_index]
+        print("Search Results:")
+        for i, result in enumerate(search_results):
+            print(f"{i+1}. {result['Name']}")
 
-    player_stats = get_player_stats(selected_player["URL"])
-    print("\nPlayer Career Stats:")
-    print(player_stats)
+        player_index = int(input("Enter the index of the player you want to analyze: ")) - 1
 
-    analyze_player_stats(player_stats)
-    visualize_player_stats(player_stats)
+        if player_index < 0 or player_index >= len(search_results):
+            print("Invalid player index.")
+            continue
 
-    export_choice = input("Do you want to export the player stats to a CSV file? (y/n): ")
-    if export_choice.lower() == "y":
-        filename = input("Enter the filename for the CSV file: ")
-        export_player_stats(player_stats, filename)
+        selected_player = search_results[player_index]
+
+        player_stats = get_player_stats(selected_player["URL"])
+        print("\nPlayer Career Stats:")
+        print(player_stats)
+
+        analyze_player_stats(player_stats)
+        visualize_player_stats(player_stats)
 
     print("Thank you for using NBA Player Stats Analyzer!")
 
